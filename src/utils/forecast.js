86b34a36448492681295cdeddb3c0f2460da0821
Utkarsh Pandey <utkarz18@gmail.com>
2019-12-05 01:38:00 +0530

    Added text to about page and new forecast text

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -14,7 +14,9 @@ const forecast = (latitude, longitude, callback) => {
             const summary = data.daily.data[0].summary;
             const temperature = data.currently.temperature
             const precipitation = data.currently.precipProbability
-            const forecastString = summary + ' Temperature is currently ' + temperature + 'C out. There is a ' + precipitation + '% chance of rain.'
+            let forecastString = summary + ' Temperature is currently ' + temperature + 'C out. There is a ' + precipitation + '% chance of rain.'
+            forecastString += ' Temperature High is ' + data.daily.data[0].temperatureHigh + 'C.'
+            forecastString += ' Temperature low is ' + data.daily.data[0].temperatureLow + 'C'
             callback(undefined, forecastString)
         }
     })
